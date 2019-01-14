af5284430c29ca43dd828754e0da6d984864d5b5
unknown <cecilephilippe31@gmail.com>
2019-01-14 12:32:07 +0100

    création des pages

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,4 +9,10 @@
 @section('content')
 
 <h1>Page d'acceuil</h1>
+
+<p class="text-left">Left aligned text on all viewport sizes.</p>
+<p class="text-center">Center aligned text on all viewport sizes.</p>
+<p class="text-right">Right aligned text on all viewport sizes.</p>
+
+
 @endsection
