28c95f100f1021b9b17ea80bed8fbe00349e6792
unknown <cecilephilippe31@gmail.com>
2019-01-14 09:27:57 +0100

    ajout de nav bar

---
+++ resources/views/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.template')
+
+@section('title', 'Page Title')
+
+@section('sidebar')
+    
+@endsection
+
+@section('content')
+
+            <nav class=" navbar navbar-expand-lg  navbar-light bg-light">
+            <a class="navbar-brand" href="#">CECILE Philippe</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav"> 
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Competences</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Formations</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Experiences</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+                
+                </ul>
+      
+    </nav>
+</div>
+@endsection
