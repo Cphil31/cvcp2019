802a9f50be06ca446f6987bb51c1109f5a572f0f
unknown <cecilephilippe31@gmail.com>
2019-01-14 17:07:09 +0100

    mise en page

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -3,7 +3,7 @@
         <title>App Name - @yield('title')</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
         <link rel="stylesheet" href="../css/style.css">
-        <link rel="stylesheet" media="screen and (max-width: 960px)" href="../css/mobile.css">
+        <link rel="stylesheet" media="screen and (max-width: 992px)" href="../css/mobile.css">
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
