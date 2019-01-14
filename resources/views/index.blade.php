94e6185a3ca33f35c90ed7d6298a0c41f49f19b4
unknown <cecilephilippe31@gmail.com>
2019-01-14 10:11:44 +0100

    nav

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,28 +8,30 @@
 
 @section('content')
 
-            <nav class=" navbar navbar-expand-lg  navbar-light bg-light">
-            <a class="navbar-brand" href="#">CECILE Philippe</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav"> 
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Competences</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Formations</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Experiences</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
-                </li>
-                
-                </ul>
-      
-    </nav>
-</div>
+<nav class="navbar  fixed-top  navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand text-uppercase nom "  href="#">cecile philippe</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse " id="navbarNav">
+    <ul class="navbar-nav nom">
+      <li class="nav-item ">
+        <a class="nav-link" href="#">Competences <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Formations</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="#">Expériences</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    
+    </ul>
+  </div>
+</nav>
+
+
+
 @endsection
