c99f4efc99d45e51a575ab048566faad979ca6aa
unknown <cecilephilippe31@gmail.com>
2019-01-15 10:12:15 +0100

    test mise en page mobile

--- resources/views/competences.blade.php
+++ resources/views/competences.blade.php
@@ -8,7 +8,7 @@
 
 @section('content')
 <div class="texte">Framework :Symfony , Laravel, Worldpress</div>
-<div class="texte">Langages  : HTML, CSS, Javasript, PHP, Mysql</div>
+<div class="texte">Langages: HTML, CSS, Javasript, PHP, Mysql</div>
 <div class="texte"> Photoshop , Illustrator</div>
 <div class="texte">Traitement de textes et Tableurs</div>
 <div class="texte">OS : Linux, Windows, Apple</div>
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -8,7 +8,7 @@
     <body>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" id="nom" href="/">Cecile Philippe Développeur</a>
-            <button id="button" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button id="button" class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
