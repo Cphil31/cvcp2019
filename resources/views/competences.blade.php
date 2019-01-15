902d5e8da7ed4e790140d3d3d8bc8b0b45a08f4d
unknown <cecilephilippe31@gmail.com>
2019-01-15 09:41:56 +0100

    test mise en page

--- resources/views/competences.blade.php
+++ resources/views/competences.blade.php
@@ -8,4 +8,9 @@
 
 @section('content')
 <div class="texte">Page Compétences </div>
+<div class="texte">Framework :Symfony , Laravel, Jquery , Worldpress</div>
+<div class="texte">Langages informatiques : HTML, CSS, Javasript, PHP, Mysql</div>
+<div class="texte">Logiciels Photoshop et Illustrator</div>
+<div class="texte">Logiciels de traitement de textes et tableurs</div>
+<div class="texte">Systèmes d'exploitation Linux, Windows, Apple</div>
 @endsection
