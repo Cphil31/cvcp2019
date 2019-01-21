abbca18ba461d4fd0a519c6448b97092abea5824
unknown <cecilephilippe31@gmail.com>
2019-01-21 12:04:22 +0100

    mise en page formulaire de contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -8,35 +8,37 @@
 
 @section('content')
 <div class="container">
+    <div class="formulaire">
 
-    <form>
-        <div class="form-group">
-            <div class="row">
-            <div class="col">
-            <label for="exampleInputName">Nom</label>
-            <input type="text" class="form-control" placeholder="Doe">
+        <form>
+            <div class="form-group">
+                <div class="row">
+                <div class="col">
+                <label for="exampleInputName">Nom</label>
+                <input type="text" class="form-control" placeholder="Doe">
+                </div>
+                <div class="col">
+                <label for="exampleInputPrenom">Prénom</label>
+                <input type="text" class="form-control" placeholder="John">
+                </div>
             </div>
-            <div class="col">
-            <label for="exampleInputPrenom">Prénom</label>
-            <input type="text" class="form-control" placeholder="John">
-            </div>
+            <label for="exampleInputEmail1">Mail</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="JohnDoe@gmail.com">
+        
         </div>
-        <label for="exampleInputEmail1">Mail</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="JohnDoe@gmail.com">
-    
-    </div>
 
-    <div class="form-group">
-    <label for="exampleFormControlTextarea1">Object</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
-    </div>
+        <div class="form-group">
+        <label for="exampleFormControlTextarea1">Object</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
+        </div>
 
-    <div class="form-group">
-    <label for="exampleFormControlTextarea1">Message</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="4"></textarea>
+        <div class="form-group">
+        <label for="exampleFormControlTextarea1">Message</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="4"></textarea>
+        </div>
+        
+        <button type="submit" class="btn btn-primary btn-lg btn-block">Envoyer</button>
+        </form>
     </div>
-    
-    <button type="submit" class="btn btn-primary btn-lg btn-block">Envoyer</button>
-    </form>
 </div>
 @endsection
