db1cebd83f24e704e717780f84813a70821de226
unknown <cecilephilippe31@gmail.com>
2019-01-21 11:46:09 +0100

    ajout du formulaire de contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,5 +7,34 @@
 @endsection
 
 @section('content')
-<div class="texte">Page Contact</div>
+<div class="container">
+
+    <form>
+        <div class="form-group">
+            <div class="row">
+            <div class="col">
+            <input type="text" class="form-control" placeholder="Nom">
+            </div>
+            <div class="col">
+            <input type="text" class="form-control" placeholder="Prénom">
+            </div>
+        </div>
+        <label for="exampleInputEmail1">Mail</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="JohnDoe@gmail.com">
+    
+    </div>
+
+    <div class="form-group">
+    <label for="exampleFormControlTextarea1">Object</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
+    </div>
+
+    <div class="form-group">
+    <label for="exampleFormControlTextarea1">Message</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+    
+    <button type="submit" class="btn btn-primary">Envoyer</button>
+    </form>
+</div>
 @endsection
